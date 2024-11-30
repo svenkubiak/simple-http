4df1fb32b6952d0cfffeff8349cff8960aa33386
Sven Kubiak <sk@svenkubiak.de>
2024-11-30 15:08:23 +0100

    Added additional unit tests

--- src/test/java/de/svenkubiak/utils/UtilsTests.java
+++ src/test/java/de/svenkubiak/utils/UtilsTests.java
@@ -1,11 +1,16 @@
 package de.svenkubiak.utils;
 
+import de.svenkubiak.http.Failsafe;
+import de.svenkubiak.http.Result;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.net.http.HttpClient;
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class UtilsTests {
 
@@ -15,13 +20,13 @@ class UtilsTests {
         List<Integer> successCodes = List.of(200, 201, 202, 203, 204, 205, 206, 207, 208, 226);
 
         //then
-        successCodes.forEach((c -> Assertions.assertTrue(Utils.isSuccessCode(c))));
+        successCodes.forEach((c -> assertThat(Utils.isSuccessCode(c)).isTrue()));
 
         //when
         int code = 302;
 
         //then
-        Assertions.assertFalse(Utils.isSuccessCode(code));
+        assertThat(Utils.isSuccessCode(code)).isFalse();
     }
 
     @Test
@@ -33,7 +38,7 @@ class UtilsTests {
         String data = Utils.getFormDataAsString(formData);
 
         //then
-        Assertions.assertTrue("username=foo&password=bar".equals(data) || "password=bar&username=foo".equals(data));
+        assertThat("username=foo&password=bar".equals(data) || "password=bar&username=foo".equals(data)).isTrue();
     }
 
     @Test
@@ -45,7 +50,23 @@ class UtilsTests {
         client = Utils.getHttpClient(true, true);
 
         //then
-        Assertions.assertNotNull(client);
-        Assertions.assertInstanceOf(HttpClient.class, client);
+        assertThat(client).isNotNull();
+        assertThat(client).isInstanceOf(HttpClient.class);
+    }
+
+    @Test
+    void testSetFailsafe() {
+        //given
+        String url = "http://localhost:8080";
+        Failsafe failsafe = Failsafe.of(5, Duration.ofMinutes(1));
+        Result result = Result.create().withStatus(200);
+
+        //when
+        Utils.setFailsafe(url, failsafe, result);
+
+        //then
+        assertThat(result).isNotNull();
+        assertThat(failsafe.getCount()).isEqualTo(1);
+        assertThat(failsafe.getUntil()).isNull();
     }
 }
